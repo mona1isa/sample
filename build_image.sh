69092890c2371f115162161c4e757aeef80a68c5
Zhany_v <zhangyu_neu@sina.com>
2019-01-16 15:50:47 +0800

    完善 build脚本

--- build_image.sh
+++ build_image.sh
@@ -3,7 +3,12 @@ cd build/docker/
 
 docker build -t sample:latest -f Dockerfile .
 
+# tag image then push
+docker tag sample:latest 172.16.20.2:8083/sample:latest
+
 # push image into registry need to do
+docker push 172.16.20.2:8083/sample:latest
+
 
 # use docker-compose startup container
 cd ../../src/main/docker
